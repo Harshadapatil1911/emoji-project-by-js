54e49533e91fb6fbcb6d66c5612f74be2c1f7da1
“Harshada_Patil” <patilharshada1911@gmail.com>
2024-01-17 23:54:37 +0530

    adding files

---
+++ script.js
@@ -0,0 +1,53 @@
+let input=document.getElementById("search_fields");
+let emoji=document.getElementById("emoji");
+// let search=document.getElementById("search")
+
+
+function displayresult(search_value=""){
+    let filtered_list=emojiList.filter((e)=>{
+        let ans=search_value.toLowerCase();
+        if(e.description.indexOf(ans) != -1){
+            return true;
+        }
+        if(e.tags.some((elem) => elem.startsWith(ans))){
+            return true;
+        }
+
+        if(e.aliases.some((elem) => elem.startsWith(ans))){
+            return true;
+        }
+    })
+
+
+
+
+
+
+// let parent=document.getElementById("emoji");
+emoji.innerHTML="";
+filtered_list.forEach((e)=>{
+let div=document.createElement("div");
+div.classList.add("emoji-card");
+div.innerHTML=`
+<p class="emoji">${e.emoji}</p>
+<p class="alises">${e.aliases[0]}</p>
+
+
+`
+// if(is.toUpperCase(e.aliases) ||  )
+emoji.appendChild(div);
+});
+
+
+}
+// console.log(emojiList);
+
+
+
+
+window.onload=()=>displayresult();
+input.addEventListener("keyup",()=>{
+    let search_value=input.value;
+    // search_value.toLowerCase();
+    displayresult(search_value);
+});
